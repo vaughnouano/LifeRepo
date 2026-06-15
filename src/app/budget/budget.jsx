d81a352d3bf052ceb4b10cd59b2335f78de1e46b
vaughnouano <vaughneurielouano@example.com>
2026-06-15 12:54:00 +0800

    Refactor: update budget component to include navigation toggle functionality and improve layout

--- src/app/budget/budget.jsx
+++ src/app/budget/budget.jsx
@@ -1,9 +1,37 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Image, Pressable, ScrollView, View } from "react-native";
+import { layout } from "../../app/home/homeStyle";
+import BottomButton from "../../components/Button/BottomButton";
+import Header from "../../components/Header/Header";
+import Navigation from "../../components/Navigation/Navigation";
+import { color } from "../../constants/color";
 
-export default function budget() {
+export default function Home() {
+  const [isNavVisible, setIsNavVisible] = useState(true);
+
+  const toggleNav = () => {
+    setIsNavVisible(!isNavVisible);
+  };
+
   return (
-    <View>
-      <Text>Budget Screen</Text>
+    <View style={[layout.container, color.bg_clr_100]}>
+      {/* Header */}
+      <Header Date="THU" DayNumber="56" style={layout.header} />
+      {/* Lists */}
+      <ScrollView style={[layout.listContainer, color.bg_clr_200]}>
+        {/* <List Task="Study Math" Time="2:00 PM" />
+        <List Task="Clean Room" Time="4:00 PM" /> */}
+      </ScrollView>
+      {/* Nav Container - Clickable */}
+      <Pressable onPress={toggleNav} style={layout.navContainer}>
+        <Image
+          style={layout.navToggleImg}
+          source={require("../../../assets/images/nav_horizontal_toggle.png")}
+        />
+        <View>{isNavVisible && <Navigation style={layout.navigation} />}</View>
+      </Pressable>
+      {/* Bottom Button */}
+      <BottomButton Title="+Task" style={layout.buttomButton} />
     </View>
   );
 }
